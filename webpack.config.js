9d6d85a88c4e3d08a63960a652e589dcc87003f5
JP LEMOUSSU <jp.lemoussu@gmail.com>
2017-01-12 23:35:14 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
